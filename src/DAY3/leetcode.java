5f8072d30c0af0a29563a39c181d8befdac3f117
Yuvansri K <ysri87923@gmail.com>
2026-07-15 21:51:02 +0530

    work

---
+++ src/DAY3/leetcode.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int total = numBottles;
+        int empty = numBottles;
+        while (empty >= numExchange) {
+            int newBottles = empty / numExchange;
+            total += newBottles;
+            empty = empty % numExchange + newBottles;
+        }
+        return total;
+    }
+}
